d02270b3657b5abf506092945be1621e4f542a55
BoBroccoli <wenxx194@d.umn.edu>
2021-04-26 19:57:22 -0400

    update download resume

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,7 +3,9 @@ import Navigation from './Navigation';
 import profileLogo from "../assets/photo.jpg"
 import '../style/home.scss'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faLinkedin, faGithub} from "@fortawesome/free-brands-svg-icons";
+import { faYoutube, faLinkedin, faGithub} from "@fortawesome/free-brands-svg-icons";
+import { faDownload} from "@fortawesome/free-solid-svg-icons";
+import FileSaver from 'file-saver'
 
 class Home extends React.Component {
     render() {
@@ -15,27 +17,27 @@ class Home extends React.Component {
                         <img src={profileLogo} alt="profile"></img>
                         <h1 className='name'>Bo Wen</h1>
                         <h2 className='name'>Software Engineer</h2>
-                        <h3 className='name'>LinkedIn: <a className='name' href="https://www.linkedin.com/in/bo-wen-a62304a8/" target="_blank" rel="noreferrer">https://www.linkedin.com/in/bo-wen-a62304a8/</a> </h3>
-                        <h3 className='name'>GitHub: <a className='name' href="https://github.com/BoBroccoli" target="_blank" rel="noreferrer">https://github.com/BoBroccoli</a> </h3>
-                        <h3 className='name'>Email: bowen4091761@gmail.com</h3>
-                        <h3 className='name'>Phone: 2184091761</h3>
                         <ul>
                             <li>
-                                <a href="www.google.com">
-                                    <FontAwesomeIcon icon={faFacebook} />
+                                <a href="https://www.youtube.com/channel/UCSEB9sZXY8rOmHTK81pbNFw" target="_blank" rel="noreferrer">
+                                    <FontAwesomeIcon icon={faYoutube} />
                                 </a>
                             </li>
                             <li>
-                                <a href="www.google.com">
+                                <a href="https://www.linkedin.com/in/bo-wen-a62304a8/" target="_blank" rel="noreferrer">
                                     <FontAwesomeIcon icon={faLinkedin} />
                                 </a>
                             </li>
                             <li>
-                                <a href="www.google.com">
+                                <a href="https://github.com/BoBroccoli" target="_blank" rel="noreferrer">
                                     <FontAwesomeIcon icon={faGithub} />
                                 </a>
                             </li>
                         </ul>
+                        <button className='downloadBt' onClick={()=> {FileSaver.saveAs("/resume.pdf", "Bo Wen Resume.pdf")}}>
+                            <FontAwesomeIcon icon={faDownload}/>
+                            <p>Resume</p>
+                        </button>
                     </div>
                 </div>
             </div>
